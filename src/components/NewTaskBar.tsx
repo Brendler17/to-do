5952bc2a0c0502c6aee93197fbd80663858ca15d
Gustavo Brendler <gmbrendler@inf.ufpel.edu.br>
2023-07-12 23:32:29 -0300

    created new task state

--- src/components/NewTaskBar.tsx
+++ src/components/NewTaskBar.tsx
@@ -1,12 +1,37 @@
-import { PlusCircle } from '@phosphor-icons/react'
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { PlusCircle } from '@phosphor-icons/react';
+import styles from './NewTaskBar.module.css';
 
-export function NewTaskBar() {
+interface NewTaskBarProps {
+  createTask: (taskTitle: string) => void;
+}
+
+export function NewTaskBar({ createTask }: NewTaskBarProps) {
+
+  const [taskTitle, setTaskTitle] = useState('');
+
+  function handleChangeTaskTitle(event: ChangeEvent<HTMLInputElement>) {
+    setTaskTitle(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    createTask(taskTitle);
+    setTaskTitle('');
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Adicione uma nova tarefa" />
+    <form className={styles.taskBarContainer} onSubmit={handleCreateNewTask}>
+      <input
+        value={taskTitle}
+        onChange={handleChangeTaskTitle}
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+        required
+      />
       <button type="submit">
-        Criar
-        <PlusCircle size={24} />
+        <p>Criar</p>
+        <PlusCircle size={20} />
       </button>
     </form>
   )
